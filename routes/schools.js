c3d81a63ddf9c68d21dca9369b6493cae2acdab0
Super Miranda <superada@fl860.local>
2018-05-23 11:25:58 +0300

    fix server crash

--- routes/schools.js
+++ routes/schools.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../data/data.json');
+
 
 router.get('/', function(req, res, next) {
-  res.json(data.schools);
+
 });
 
-router.get('/search/:name', function(req, res, next) {
-  var matches = data.schools.filter(function (school) {
-    return school.name.toLowerCase().includes(req.params.name.toLowerCase());
-  });
-  res.json(matches);
-});
+
 
 module.exports = router;
