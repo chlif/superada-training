de8ff1f98d15bf77212923fa97ff60f94e690f29
superuuna <uuna.saarela@gmail.com>
2018-05-23 14:27:29 +0300

    index comma

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Haku'; survey: survey});
+  res.render('index', { title: 'Haku', survey: survey});
 });
 
 module.exports = router;
