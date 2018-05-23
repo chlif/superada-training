c3df0e4b4f2d91552f0da086afef309b3be0b838
Super Miranda <superada@fl860.local>
2018-05-23 13:17:59 +0300

    fix survey.js and survey.json

--- routes/survey.js
+++ routes/survey.js
@@ -2,16 +2,15 @@ var express = require('express');
 var router = express.Router();
 var questions = require('../data/survey.json');
 var matrix = require('../data/matrix.json');
+var survey = {
+    data: questions,
+    matrix: matrix
+};
 
 router.get('/', function(req, res, next) {
-  res.json(questions.questions);
+  res.json(survey);
 });
 
-router.get('/search/:name', function(req, res, next) {
-  var matches = guestions.guestions.filter(function (question) {
-    return question.text.toLowerCase().includes(req.params.name.toLowerCase());
-  });
-  res.json(matches);
-});
+
 
 module.exports = router;
