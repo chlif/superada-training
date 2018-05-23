c35ca5d49fd0e1e22603297c93f1e7b906c8c956
Super Miranda <superada@fl860.local>
2018-05-23 14:48:46 +0300

    tinker with index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var questions = require('../data/survey.json');
+var matrix = require('../data/matrix.json');
+var survey = {
+    data: questions,
+    matrix: matrix
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
