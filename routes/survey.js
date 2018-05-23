bcf2464ee32a895d3c57535a9a8a20029900e704
Super Miranda <superada@fl860.local>
2018-05-23 12:56:58 +0300

    work on survey.js

--- routes/survey.js
+++ routes/survey.js
@@ -1,3 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../data/survey.json');
+var questions = require('../data/survey.json');
+var matrix = require('../data/matrix.json');
+
+router.get('/', function(req, res, next) {
+  res.json(questions.questions);
+});
+
+router.get('/search/:name', function(req, res, next) {
+  var matches = guestions.guestions.filter(function (question) {
+    return question.text.toLowerCase().includes(req.params.name.toLowerCase());
+  });
+  res.json(matches);
+});
+
+module.exports = router;
