951bb15d2e416b7180a9cddf342f91ded3822008
Super Miranda <superada@fl860.local>
2018-05-24 14:52:39 +0300

    add logic and answer collection to le prog

--- public/javascripts/survey.js
+++ public/javascripts/survey.js
@@ -1,8 +1,35 @@
 (function($) {
-  $(document).ready(function() {
+  var survey;
+  var activeQuestion = -1;
+  var userSelection = [0,0,0,0,0,0,0,0,0,0,0,0];
 
+  $(document).ready(function() {
+    $.get('/survey/', function(data){
+      survey = data;
+    }).fail(function(error){
+      console.log("ERROR", error);
+    });
   });
-  $(button).click(function(event) {
-    if($(event.target).data("buttonName") == "start"){
-      alert("hello");
+
+
+  $("button").click(function(event) {
+    var buttonName = $(event.target).data("buttonName");
+    if(buttonName === "start"){
+      nextSlide();
+    } else {
+      addUserSelection(buttonName);
+      nextSlide();
     };
+  });
+
+  function nextSlide() {
+    activeQuestion++;
+    $('#carouselExampleControls').carousel('next');
+  }
+
+  function addUserSelection(buttonName) {
+    var index = survey.matrix.keywords.indexOf(buttonName);
+    userSelection[index] = 1;
+  }
+
+})(jQuery);
